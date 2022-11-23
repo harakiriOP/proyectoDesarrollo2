649a2f700a059259824302cbbb41862744e2d0b4
Salvador Hidalgo <salvadorhidalgo100@gmail.com>
2022-11-23 16:26:12 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="card">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p></p>
+          <form>
+            <input type="email" placeholder="Usuario"/>
+            <p></p>
+            <input type="password" placeholder="Contraseña"></input>
+            <p></p>
+            <button type="submit"><b>Ingresar</b></button>
+          </form>
+        </div>
       </header>
     </div>
   );
